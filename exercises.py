38d9b10b20e7a10b41559f695ef1dbb004df22f3
davidneistadt <david.neistadt@gmail.com>
2018-02-10 17:06:25 -0500

    exercise 12

--- exercises.py
+++ exercises.py
@@ -144,6 +144,26 @@ def exercise11():
         print(number, 'is NOT a prime number!')
 
 
+def exercise12():
+    choice = ''
+    choices = []
+    while choice != 'done':
+        choice = input('Provide a number. Type "done" to stop: ')
+        if choice.isnumeric():
+            choices.append(choice)
+        elif choice != 'done':
+            print('Not a number or the word "done". Try again.')
+        else:
+            print('Finished List')
+
+    print('Original List is:', choices)
+    if len(choices) > 0:
+        first_and_last = [choices[0], choices[-1]]
+        print('First and Last elements are: ', first_and_last)
+    else:
+        print('Not enough elements')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Execute Learning Python exercises')
     parser.add_argument('exercise', action='store', type=int)
