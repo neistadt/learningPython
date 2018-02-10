7c61d749d54248d50316d6b0e399f867ece1911f
davidneistadt <david.neistadt@gmail.com>
2018-02-10 17:20:04 -0500

    exercise 14

--- exercises.py
+++ exercises.py
@@ -178,6 +178,25 @@ def exercise13():
     print('Fibonacci sequence:', fibonacci)
 
 
+def exercise14():
+    a = [1, 2, 3, 4, 5, 5, 1, 4, 4, 5, 3, 0]
+    print('Original:             ', a)
+    print('Removed (using lists):', remove_duplicates_using_lists(a))
+    print('Removed (using sets): ', remove_duplicates_using_sets(a))
+
+
+def remove_duplicates_using_lists(list_to_examine):
+    result = []
+    for x in list_to_examine:
+        if x not in result:
+            result.append(x)
+    return result
+
+
+def remove_duplicates_using_sets(list_to_examine):
+    return list(set(list_to_examine))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Execute Learning Python exercises')
     parser.add_argument('exercise', action='store', type=int)
